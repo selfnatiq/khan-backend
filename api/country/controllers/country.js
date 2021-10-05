acb413aa83e91fe85c79e6bd85fc2cba4bbbf670
Mumtaz Natiq, GHR-OSA-NEX-21 <Natiq.Mumtaz@swisscom.com>
2021-10-05 22:27:57 +0200

    feat: return all countries

--- api/country/controllers/country.js
+++ api/country/controllers/country.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.country.search(ctx.query);
+    } else {
+      entities = await strapi.services.country.find({ _limit: -1 });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.country })
+    );
+  },
+};
