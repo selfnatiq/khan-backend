bbee79c750559edadc44b15fbcf691805e9c1567
Mumtaz Natiq, GHR-OSA-NEX-21 <Natiq.Mumtaz@swisscom.com>
2021-10-12 18:24:59 +0200

    feat: remove limit from city and country

--- api/country/controllers/country.js
+++ api/country/controllers/country.js
@@ -5,26 +5,4 @@
  * to customize this controller
  */
 
-const { sanitizeEntity } = require("strapi-utils");
-
-module.exports = {
-  /**
-   * Retrieve records.
-   *
-   * @return {Array}
-   */
-
-  async find(ctx) {
-    let entities;
-
-    if (ctx.query._q) {
-      entities = await strapi.services.country.search(ctx.query);
-    } else {
-      entities = await strapi.services.country.find({ _limit: -1 });
-    }
-
-    return entities.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models.country })
-    );
-  },
-};
+module.exports = {};
